7a37add3ddf1de4d984a9eccf9b27103e496fff2
roger alvarez <rogeralvarez3@gmail.com>
2020-07-25 12:15:45 -0600

    3er commit

--- api.js
+++ api.js
@@ -1,24 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const io = require("socket.io")
+const io = require("socket.io");
+const db = require("./db");
 
 app.use(cors());
 app.use(bodyParser());
 
-const port = process.env.port || "3000"
+app.post("login", (req, res) => {});
+app.post("list", (req, res) => {});
+app.post("save", (req, res) => {});
+app.post("delete", (req, res) => {});
+
+const port = process.env.port || "3000";
 let svr = app.listen(port, () => {
-    console.log(`Server started on http://localhost:${port}`);
+  console.log(`Server started on http://localhost:${port}`);
 });
 
 let socket = io.listen(svr);
-socket.on("connection",sck=>{
-    console.log(`Cliente ${sck.id} conectado`)
-    sck.on("disconnect",()=>{
-        console.log(`Cliente ${sck.id} desconectado`)
-    })
-})
-
-
-
+socket.on("connection", (sck) => {
+  console.log(`Cliente ${sck.id} conectado`);
+  sck.on("disconnect", () => {
+    console.log(`Cliente ${sck.id} desconectado`);
+  });
+});
